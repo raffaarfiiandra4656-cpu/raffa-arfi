e5af756d0139750f80f3f2d84ba0d604b19e5133
raffa <raffaarfiiandra@gmail.com>
2026-06-09 00:37:20 +0700

    fix: hardcode correct Supabase project credentials in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: 'https://ropeldysyiqopixcrffy.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcGVsZHlzeWlxb3BpeGNyZmZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4Mzc4ODYsImV4cCI6MjA5NjQxMzg4Nn0.ByjdvPzNLr-fawtrm__gSOigd3A5BnG9safovY5S8Dk',
+  },
 };
 
 export default nextConfig;
